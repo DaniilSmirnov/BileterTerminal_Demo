2926979a27bbab539e59c83b53879ee56d5ee66e
DaniilSmirnov <littlebigcrafting@gmail.com>
2018-12-09 13:41:29 +0300

    начали вязать запросы

--- admin.py
+++ admin.py
@@ -6,8 +6,12 @@ cnx = mysql.connector.connect(user='root', password='i130813',
                               database='mydb')
 cnx.close()
 
+'''
+
+'''
 
 class Ui_MainWindow(object):
+
     def setupUi(self):
         MainWindow.setObjectName("MainWindow")
         MainWindow.showFullScreen()
@@ -46,12 +50,72 @@ class Ui_MainWindow(object):
         self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
+    def setupMainUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.showFullScreen()
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.verticalLayout = QtWidgets.QVBoxLayout(self.centralwidget)
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.scrollArea = QtWidgets.QScrollArea(self.centralwidget)
+        self.scrollArea.setWidgetResizable(True)
+        self.scrollArea.setObjectName("scrollArea")
+        self.scrollAreaWidgetContents = QtWidgets.QWidget()
+        self.scrollAreaWidgetContents.setGeometry(QtCore.QRect(0, 0, 555, 291))
+        self.scrollAreaWidgetContents.setObjectName("scrollAreaWidgetContents")
+        self.gridLayout = QtWidgets.QGridLayout(self.scrollAreaWidgetContents)
+        self.gridLayout.setObjectName("gridLayout")
+        self.lineEdit = QtWidgets.QLineEdit(self.scrollAreaWidgetContents)
+        self.lineEdit.setObjectName("lineEdit")
+        self.gridLayout.addWidget(self.lineEdit, 0, 0, 1, 1)
+        self.lineEdit_2 = QtWidgets.QLineEdit(self.scrollAreaWidgetContents)
+        self.lineEdit_2.setObjectName("lineEdit_2")
+        self.gridLayout.addWidget(self.lineEdit_2, 0, 1, 1, 1)
+        self.lineEdit_3 = QtWidgets.QLineEdit(self.scrollAreaWidgetContents)
+        self.lineEdit_3.setObjectName("lineEdit_3")
+        self.gridLayout.addWidget(self.lineEdit_3, 0, 2, 1, 1)
+        self.scrollArea.setWidget(self.scrollAreaWidgetContents)
+        self.verticalLayout.addWidget(self.scrollArea)
+        self.savebutton = QtWidgets.QPushButton(self.centralwidget)
+        self.savebutton.setObjectName("savebutton")
+        self.verticalLayout.addWidget(self.savebutton)
+        self.cancelbutton = QtWidgets.QPushButton(self.centralwidget)
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.cancelbutton.setObjectName("cancelbutton")
+        self.verticalLayout.addWidget(self.cancelbutton)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 575, 21))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateMainUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateMainUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.savebutton.setText(_translate("MainWindow", "Save"))
+        self.cancelbutton.setText(_translate("MainWindow", "Cancel"))
+
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
         self.label_2.setText(_translate("MainWindow", "Login"))
         self.label.setText(_translate("MainWindow", "Password"))
         self.pushButton.setText(_translate("MainWindow", "Войти"))
+
+        self.pushButton.clicked.connect(self.login)
+
+    def login(self):
+        username_entered = self.label2.text()
+        password_entered = self.label.text()
+
+        query = ("SELECT ")
+
+        cnx.execute(query)
 
 
 if __name__ == "__main__":
